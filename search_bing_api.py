46743e26eaffac98e7eebbe197ac92be0eb9bb74
cmlinac <cmlinac@gmail.com>
2020-06-13 22:50:05 -0500

    initial commit

---
+++ search_bing_api.py
@@ -0,0 +1,14 @@
+# import the necessary packages
+from requests import exceptions
+import argparse
+import requests
+import cv2
+import os
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-q", "--query", required=True,
+                help="search query to search Bing Image API for")
+ap.add_argument("-o", "--output", required=True,
+                help="path to output directory of images")
+args = vars(ap.parse_args())
